25b9dd60f7b02cf7e77b2dffc1c10ae4c69f77e2
Brian Kennedy <bpkennedy@gmail.com>
2021-05-03 10:09:27 -0500

    Rollback auto-import chakra change

--- nuxt.config.js
+++ nuxt.config.js
@@ -80,9 +80,6 @@ export default {
 
   // Chakra UI Configuration
   chakra: {
-    config: {
-      autoImport: false,
-    },
     icons: {
       // Here we state that we use `fa`
       // icons library for Chakra's
